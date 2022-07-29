676769cdf058b702f3e6528c81291b7d95f058ab
Jack Piet Thyrie <jpt@semaphor.dk>
2022-07-29 15:12:35 +0200

    End of section 4

---
+++ Application/Activities/Details.cs
@@ -0,0 +1,31 @@
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Details
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext content;
+
+            public Handler(DataContext content)
+            {
+                this.content = content;
+            }
+
+            
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                
+                return await this.content.Activities.FindAsync(request.Id);
+            }
+        }
+    }
+}
