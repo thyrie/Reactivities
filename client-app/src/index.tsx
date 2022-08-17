01e75bffebc3fb140d604056903dcd5c8d9c54a4
Jack Piet Thyrie <jpt@semaphor.dk>
2022-08-17 15:04:49 +0200

    End of sekction 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <StoreContext.Provider value={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StoreContext.Provider>
+  </StoreContext.Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
